26590fa8141e4f75a474263a832ab8d91b32c783
nquinn10 <ntquinn10@gmail.com>
2024-01-30 13:11:33 -0500

    notes on reactDOM

--- scripts/reactDOM.js
+++ scripts/reactDOM.js
@@ -1,14 +1,19 @@
 window.onload = function() {
     document.getElementById('myInp').onkeyup = enter;
     document.getElementById('myBtn').onclick = fillMsg;
+    // when page loads - regualr function that retrieves HTML element
+    // when even happens on that element - then we want to call function
+    // defined below
 }
 
 function enter(event) {
     if(event.key === 'Enter') {
         document.getElementById('myBtn').click();
+        // if enter - retirves button element and click
     }
 }
 
 function fillMsg() {
     document.getElementById('msg').textContent = 'Button Clicked!'
+    // retrieves another element and sets text content in DOM
 }
