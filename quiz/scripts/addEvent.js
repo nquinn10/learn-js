0dffc12c45e547de3d5bf54ad78d5b30caa0f6b6
nquinn10 <ntquinn10@gmail.com>
2024-01-30 13:20:30 -0500

    notes to addEvent.js

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -1,18 +1,21 @@
 window.onload = function() {
     let x = document.getElementById('myBtn');
-    x.addEventListener('mouseover', myFunction('Moused over!'));
+    x.addEventListener('mouseover', myFunction('Moused over!')); // can update to nested function that returns closure
     x.addEventListener('click', mySecondFunction('Clicked!'));
     x.addEventListener('mouseout', myThirdFunction('Moused out!'));
 }
 
 function myFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
+    // update to include arrow function
 }
 
 function mySecondFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () =>document.getElementById('demo').textContent = msg;
 }
 
 function myThirdFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
+
+// problem is that functions are not returning anything - need closure
