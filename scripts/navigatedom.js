50b5266ee6c1b246b23d7c62ae56635fb014c753
nquinn10 <ntquinn10@gmail.com>
2024-01-30 13:26:08 -0500

    notes to navigate dom

--- scripts/navigatedom.js
+++ scripts/navigatedom.js
@@ -1,6 +1,8 @@
 function logCells() {
   // Get all td elements inside the table within the div with id "root"
   var tdElements = document.querySelectorAll('#root table tr td');
+  // use query selector to get all columns
+  // gives list of td elements of node type
   // Log the td elements
   tdElements.forEach(function(td, index) {
     console.log(` ==== Index === ${index}`);
@@ -8,6 +10,7 @@ function logCells() {
     console.log(`Node Value = ${td.childNodes[0].nodeValue}`);
     console.log(`Node innerHTML = ${td.innerHTML}`);
     console.log(`Node text = ${td.textContent}`);
+    // going through td events and logging each item
   });
 
 }
